551712d8fc1d9bdf7fe3cbffa06356d0bfe62211
boxer <fzhong.li@gmail.com>
2018-04-17 00:12:34 +0800

    尝试动态注册路由

--- application/route.php
+++ application/route.php
@@ -9,13 +9,32 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
 
-];
+// 配置式的路由器写法
+//return [
+//    '__pattern__' => [
+//        'name' => '\w+',
+//    ],
+//    '[hello]'     => [
+//        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+//        ':name' => ['index/hello', ['method' => 'post']],
+//    ],
+//
+//];
+
+
+
+//动态注册的路由器写法
+//动态注册
+//
+//路由定义采用\think\Route类的rule方法注册，通常是在应用的路由配置文件application/route.php进行注册，格式是：
+//
+//Route::rule('路由表达式','路由地址','请求类型','路由参数（数组）','变量规则（数组）');
+//例如注册如下路由规则：
+//
+//use think\Route;
+//// 注册路由到index模块的News控制器的read操作
+//Route::rule('new/:id','index/News/read');
+
+use think\Route;
+Route::rule('hello','sample\Test\hello','POST|GET');
